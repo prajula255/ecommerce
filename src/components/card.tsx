d49ee14b3108179b4b2dbb0297e10dac2579009b
prajula255 <prajulasouparnika3@gmail.com>
2024-12-17 10:10:57 +0530

    card page updated

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -14,7 +14,7 @@ interface CardegProps {
 }
 
 const Cardeg: FC<CardegProps> = (props) => {
-  const {  image, title } = props.product
+  const { category,description,id, image,price, title } = props.product
 
   return (
     <>
