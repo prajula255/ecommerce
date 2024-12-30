6ad96646662ba7382e5f59aca0ee1e3d46a8ad18
prajula255 <prajulasouparnika3@gmail.com>
2024-12-30 10:33:50 +0530

    cart page updated with product details

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,96 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import EkartPage from './pages/ekartpage'
-import CartPage from './pages/cartpage'
-function App() {
-  const [product, setProduct] = useState([])
-  const [isMobile, setIsMobile] = useState(false)
-  const [cartDetails, setCartDetails] = useState<any>([])
+// import { useEffect, useState } from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import EkartPage from './pages/ekartpage'
+// import CartPage from './pages/cartpage'
+// function App() {
+//   const [product, setProduct] = useState([])
+//   const [isMobile, setIsMobile] = useState(false)
+//   const [cartDetails, setCartDetails] = useState<any>([])
 
+//   useEffect(() => {
+//     fetch('https://fakestoreapi.com/products')
+//       .then(res => res.json())
+//       .then(json => setProduct(json))
+//   }, [])
+
+//   useEffect(() => {
+//     console.log(product);
+
+//   }, [product])
+
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth <= 768);
+//     };
+//     handleResize();
+
+//     window.addEventListener('resize', handleResize);
+
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     console.log('Is Mobile:', isMobile);
+//   }, [isMobile]);
+
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<EkartPage products={product} setCartDetails={setCartDetails} />} />
+
+
+//           <Route
+//             path="/cart"
+//             element={<CartPage cartDetails={cartDetails} />}
+//           />
+//         </Routes></BrowserRouter>
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EkartPage from './pages/ekartpage';
+import CartPage from './pages/cartpage';
+import CartDetails from './pages/cartdet';
+function App() {
+  const [products, setProducts] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
+  const [cartDetails, setCartDetails] = useState<any>([]);
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
-      .then(res => res.json())
-      .then(json => setProduct(json))
-  }, [])
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
 
   useEffect(() => {
-    console.log(product);
-
-  }, [product])
-
+    console.log('Products:', products);
+  }, [products]);
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
     };
-    handleResize();
 
+    handleResize();
     window.addEventListener('resize', handleResize);
 
     return () => {
@@ -36,28 +102,24 @@ function App() {
     console.log('Is Mobile:', isMobile);
   }, [isMobile]);
 
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<EkartPage products={product} setCartDetails={setCartDetails} />} />
-
-
           <Route
-            path="/cart"
-            element={<CartPage cartDetails={cartDetails} />}
+            path="/"
+            element={<EkartPage products={products} setCartDetails={setCartDetails} />}
           />
-        </Routes></BrowserRouter>
+
+          {/* Cart page displaying all items added */}
+          <Route path="/cart" element={<CartPage cartDetails={cartDetails} />} />
+
+          {/* Product details page for a specific cart item */}
+          <Route path="/cart-details" element={<CartDetails />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
-
-
-
-
-
-
-
---
+++ src/pages/cartdet.tsx
@@ -0,0 +1,50 @@
+// import { FC } from 'react';
+// import { useLocation } from 'react-router-dom';
+
+// const CartDetails: FC = () => {
+//   const location = useLocation();
+//   const { product } = location.state || {};
+
+//   if (!product) {
+//     return <div>No product details available</div>;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Product Details</h1>
+//       <img src={product.image} alt={product.title} style={{ width: '200px', height: '200px' }} />
+//       <h2>{product.title}</h2>
+//     </div>
+//   );
+// };
+
+// export default CartDetails;
+
+
+
+
+
+
+import { FC } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const CartDetails: FC = () => {
+    const location = useLocation();
+    const { product } = location.state || {};
+
+    if (!product) {
+        return <div>No product details available</div>;
+    }
+
+    return (
+        <div>
+            <h1>Product Details</h1>
+            <img src={product.image} alt={product.title} style={{ width: '200px', height: '200px' }} />
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            {/* <p>Price: ${product.price}</p> */}
+        </div>
+    );
+};
+
+export default CartDetails;
