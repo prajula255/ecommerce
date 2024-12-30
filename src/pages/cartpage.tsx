2ac4e91a17e370f408f04fe6217f8e1ae820a5fb
prajula255 <prajulasouparnika3@gmail.com>
2024-12-30 11:44:17 +0530

    cart page updated

--- src/pages/cartpage.tsx
+++ src/pages/cartpage.tsx
@@ -1,49 +1,37 @@
 import { FC } from "react";
 
 interface CartPageProps {
-    cartDetails: {
-        title: string,
-        image: string
-    }[];
+  cartDetails: {
+    title: string,
+    image: string
+  }[];
 }
 
-const CartPage: FC<CartPageProps> = ({ cartDetails })=> {
-    return (
-        <>
-            <div>
-                <h1>Cart Details</h1>
-                {cartDetails.length === 0 ? (
-                    <p>No items in the cart.</p>
-                ) : (
-                    <div className="cart-items">
-                        {cartDetails.map((item, index) => (
-                            <div key={index} className="cart-item">
-                                <h2>{item.title}</h2>
-                                <img
-                                    src={item.image}
-                                    alt={item.title}
-                                    style={{ width: '100px', height: 'auto' }}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
-        
-        </>
-    );
+const CartPage: FC<CartPageProps> = ({ cartDetails }) => {
+  return (
+    <>
+      <div>
+        <h1>Cart Details</h1>
+        {cartDetails.length === 0 ? (
+          <p>No items in the cart.</p>
+        ) : (
+          <div className="cart-items">
+            {cartDetails.map((item, index) => (
+              <div key={index} className="cart-item">
+                <h2>{item.title}</h2>
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  style={{ width: '100px', height: 'auto' }}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+    </>
+  );
 };
 
 export default CartPage;
-
-
-
-
-
-
-
-
-
-
-
-
--- src/pages/ekartpage.tsx
+++ src/pages/ekartpage.tsx
@@ -1,22 +1,68 @@
+// import { FC } from 'react';
+// import FooterC from '../components/footer';
+// import Cardeg from '../components/card';
+// import NavBar from '../components/navbar';
+
+// interface EkartPageProps {
+//   products:string[],
+//   setCartDetails:(value:any)=>void
+// }
+
+// const EkartPage: FC<EkartPageProps> = ({products,setCartDetails}) => {
+
+//   return (
+//     <>
+//        <NavBar />
+//       <div className='d-flex flex-wrap justify-content-around flex-row container'>
+//         {
+//           products.length > 0 ?
+//             products.map((item:string, index:number) => (
+//               <div className=" p-2" >
+//                 <Cardeg key={index + "product"} product={item} setCartDetails={setCartDetails} />
+//               </div>
+//             )) :
+//             (
+//               <div>
+//                 <p>no products available</p>
+//               </div>
+//             )
+//         }
+//       </div>
+//       <footer> < FooterC/></footer>
+
+
+//     </>
+//   );
+// };
+
+// export default EkartPage;
+
+
+
+
+
+
+
+
 import { FC } from 'react';
 import FooterC from '../components/footer';
 import Cardeg from '../components/card';
 import NavBar from '../components/navbar';
 
 interface EkartPageProps {
-  products:string[],
-  setCartDetails:(value:any)=>void
+  products: string[],
+  setCartDetails: (value: any) => void
 }
 
-const EkartPage: FC<EkartPageProps> = ({products,setCartDetails}) => {
+const EkartPage: FC<EkartPageProps> = ({ products, setCartDetails }) => {
 
   return (
     <>
-       <NavBar />
+      <NavBar />
       <div className='d-flex flex-wrap justify-content-around flex-row container'>
         {
           products.length > 0 ?
-            products.map((item:string, index:number) => (
+            products.map((item: string, index: number) => (
               <div className=" p-2" >
                 <Cardeg key={index + "product"} product={item} setCartDetails={setCartDetails} />
               </div>
@@ -28,7 +74,7 @@ const EkartPage: FC<EkartPageProps> = ({products,setCartDetails}) => {
             )
         }
       </div>
-      <footer> < FooterC/></footer>
+      <footer> < FooterC /></footer>
 
 
     </>
@@ -36,11 +82,3 @@ const EkartPage: FC<EkartPageProps> = ({products,setCartDetails}) => {
 };
 
 export default EkartPage;
-
-
-
-
-
-
-
-
