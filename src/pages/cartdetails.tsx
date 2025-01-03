50e8d3ef4b67add90883c8f7f7d9a5ed768b095a
prajula255 <prajulasouparnika3@gmail.com>
2025-01-03 11:19:46 +0530

    cartdetails updated

--- src/pages/cartdetails.tsx
+++ src/pages/cartdetails.tsx
@@ -1,41 +1,41 @@
 
-import { FC } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-interface CartDetailsProps{
-cartDetails:any
-}
-const CartDetails: FC<CartDetailsProps> = ({cartDetails}) => {
-  const location = useLocation();
-  const navigate = useNavigate();
- 
+// import { FC } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// interface CartDetailsProps{
+// cartDetails:any
+// }
+// const CartDetails: FC<CartDetailsProps> = ({cartDetails}) => {
 
-  if (!cartDetails) {
-    return <div>No product details available</div>;
-  }
+//   const navigate = useNavigate();
 
-  const handleAddToCart = () => {
+
+//   if (!cartDetails) {
+//     return <div>No product details available</div>;
+//   }
+
+//   const handleAddToCart = () => {
     
-    navigate('/cart', { state: { cartDetails } }); 
-  };
-console.log(cartDetails)
-  return (
-    <div>
-      {/* {
-        <div>
-           <h1>Product Details</h1>
-      <img
-        src={product.image}
-        alt={product.title}
-        style={{ width: '200px', height: '200px' }}
-      />
-      <h2>{product.title}</h2>
-      <p>{product.description}</p>
-      <button onClick={handleAddToCart}>Add to Cart</button>
-        </div>
-      } */}
+//     navigate('/cart', { state: { cartDetails } }); 
+//   };
+// console.log(cartDetails)
+//   return (
+//     <div>
+//       {/* {
+//         <div>
+//            <h1>Product Details</h1>
+//       <img
+//         src={product.image}
+//         alt={product.title}
+//         style={{ width: '200px', height: '200px' }}
+//       />
+//       <h2>{product.title}</h2>
+//       <p>{product.description}</p>
+//       <button onClick={handleAddToCart}>Add to Cart</button>
+//         </div>
+//       } */}
       
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default CartDetails;
+// export default CartDetails;
